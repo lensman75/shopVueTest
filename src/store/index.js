1d9fa8fa2b5a457f31468cf43d19b6e5e51bb0ee
Alex <anministry.mgmt@gmail.com>
2021-05-18 12:11:40 +0300

    add profile

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    avatar: 'https://picsum.photos/seed/picsum/200/300',
+    fullname: 'Vasya Petrov',
+    age: 30,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="profile">
     <h1>This is a profile page.</h1>
+    <div>Name: {{$store.state.fullname}}</div>
+    <div>Age: {{$store.state.age}}</div>
+    <img style="height: 100px; width: 100px" v-bind:src="$store.state.avatar">
   </div>
 </template>
